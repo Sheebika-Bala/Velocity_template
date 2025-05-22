2fae95a348f18cf6f4802821dc199d0464cc7278
Sheebika-Bala <128901514+Sheebika-Bala@users.noreply.github.com>
2025-05-22 15:00:01 +0530

    Update build.gradle.kts

--- VelTemp/build.gradle.kts
+++ VelTemp/build.gradle.kts
@@ -13,7 +13,6 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.apache.velocity:velocity-engine-core:2.3")
-    implementation("org.slf4j:slf4j-simple:2.0.13")
 }
 
 tasks.test {
